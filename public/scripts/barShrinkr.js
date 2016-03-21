6a60dec7a599b9b6d3df5882f8fcf759d8c03fe2
micklethepickle <michel_ma@hotmail.ca>
2016-03-20 21:40:19 -0400

    deuce

--- public/scripts/barShrinkr.js
+++ public/scripts/barShrinkr.js
@@ -1,8 +1,12 @@
             function shrinkH(){
-            	var header = document.getElementById('header');
             	if (window.pageYOffset <=640){
             		var header = document.getElementById('header');
+                    var bigLogo = document.getElementById('bigLogo');
+                    var h1 = document.getElementById('h1');
                 	header.style.height = (700 - window.pageYOffset) + 'px';
+                    bigLogo.style.top = (200 - window.pageYOffset) + 'px';
+                    bigLogo.style.opacity = (1 - window.pageYOffset/450);
+                    h1.style.opacity = (window.pageYOffset/450);
                 	} else if (window.pageYOffset > 640){
             			var header = document.getElementById('header');
             			header.style.height = 60 + 'px';
