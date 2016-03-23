6ac817309c2f6032fd4d1a755bd3358e030b12aa
Silan He <silanhe@outlook.com>
2016-03-22 22:50:21 -0400

    added objective and capabilities images

--- public/scripts/boxGrower.js
+++ public/scripts/boxGrower.js
@@ -2,12 +2,16 @@
 $(function() {
 	$('.boxText').on('click', function(){
 		var boxid = $(this).attr('data-boxid');
-		if ($(this).css("top") == '-580px'){
+		var imageid = $(this).attr('data-imageid')
+		if ($(this).css("top") == '-580px'){//open
 			$(this).animate({top: '-=80px'}, 400);
 			$('#'+boxid).delay(400).slideToggle(700);
-		} else {
+			$('#'+imageid).animate({right:'300px', top:'-=80px'}, 600);
+		} else {//close
 			$('#'+boxid).slideToggle(700);
 			$(this).delay(700).animate({top: '+=80px'}, 400);
+			$('#'+imageid).animate({right:'0px', top:'+=80px'}, 600);
+
 		};
 
 	});
