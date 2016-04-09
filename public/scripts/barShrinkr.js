9b83d50b92a4369e1f6764fad26b004910e2c93d
Silan He <silanhe@outlook.com>
2016-04-09 00:09:11 -0400

    did initial conversion from px to em with font size 20. Conversion factor is 0.05. Changed bar shrinker and style.styl.

--- public/scripts/barShrinkr.js
+++ public/scripts/barShrinkr.js
@@ -1,14 +1,14 @@
             function shrinkH(){
-            	if (window.pageYOffset <=640){
+            	if (0.05*window.pageYOffset <=32){
             		var header = document.getElementById('header');
                     var bigLogo = document.getElementById('bigLogo');
                     var h1 = document.getElementById('h1');
-                	header.style.height = (700 - window.pageYOffset) + 'px';
-                    bigLogo.style.opacity = (1 - window.pageYOffset/450);
-                    h1.style.opacity = (window.pageYOffset/450);
-                	} else if (window.pageYOffset > 640){
+                	header.style.height = (35 - 0.05*window.pageYOffset) + 'em';
+                    bigLogo.style.opacity = (1 - 0.05*window.pageYOffset/22.5);
+                    h1.style.opacity = (0.05*window.pageYOffset/22.5);
+                	} else if (0.05*window.pageYOffset > 32){
             			var header = document.getElementById('header');
-            			header.style.height = 60 + 'px';
+            			header.style.height = 3 + 'em';
             			}
             	}
             window.addEventListener("scroll", shrinkH, false);
--- public/scripts/boxGrower.js
+++ public/scripts/boxGrower.js
@@ -3,7 +3,7 @@ $(function() {
 	$('.boxText').on('click', function(){
 		var boxid = $(this).attr('data-boxid');
 		var imageid = $(this).attr('data-imageid')
-		if ($(this).css("top") == '-580px'){//open
+		if ($(this).css("top") >= '-580px'){//open
 			$(this).animate({top: '-=80px'}, 400);
 			$('#'+boxid).delay(400).slideToggle(700);
 			$('#'+imageid).animate({right:'300px', top:'-=80px'}, 600);
