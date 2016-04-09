a03648e44d7bb3e64e141022351a5cb80cf5fdbc
Silan He <silanhe@outlook.com>
2016-04-09 11:41:41 -0400

    made initial fix to capabilites and objectives box animations, help me fix the condition for shrinking and enlarging boxes michel

--- public/scripts/boxGrower.js
+++ public/scripts/boxGrower.js
@@ -3,14 +3,14 @@ $(function() {
 	$('.boxText').on('click', function(){
 		var boxid = $(this).attr('data-boxid');
 		var imageid = $(this).attr('data-imageid')
-		if ($(this).css("top") >= '-580px'){//open
-			$(this).animate({top: '-=80px'}, 400);
+		if ($(this).css("top")>='-28em'){//open how do i fix thisssssss??????
+			$(this).animate({top: '-=3em'}, 400);
 			$('#'+boxid).delay(400).slideToggle(700);
-			$('#'+imageid).animate({right:'300px', top:'-=80px'}, 600);
+			$('#'+imageid).animate({right:'15em', top:'-=3em'}, 600);
 		} else {//close
 			$('#'+boxid).slideToggle(700);
-			$(this).delay(700).animate({top: '+=80px'}, 400);
-			$('#'+imageid).animate({right:'0px', top:'+=80px'}, 600);
+			$(this).delay(700).animate({top: '+=3em'}, 400);
+			$('#'+imageid).animate({right:'0', top:'+=3em'}, 600);
 
 		};
 
